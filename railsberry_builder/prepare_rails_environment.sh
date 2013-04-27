b981e0f0d8999394f0ef36c6452038b5a8001c39
Anders Sondergaard Jensen <anders.soendergaard@gmail.com>
2013-04-27 10:04:56 +0200

    Escape curly braces

--- railsberry_builder/prepare_rails_environment.sh
+++ railsberry_builder/prepare_rails_environment.sh
@@ -76,13 +76,13 @@ setup_passenger () {
   sudo mkdir -p /opt/nginx/conf 2>/dev/null
   sudo chown denviro:users /opt/nginx/conf
   echo "
-  server {
+  server \{
   listen 80;
   server_name m23.merlose.dk;
   rails_env production;
   root /var/rails_apps/sample_app/public; # <--- be sure to point to 'public'!
   passenger_enabled on;
-  }" >> /opt/nginx/conf/nginx.conf
+  \}" >> /opt/nginx/conf/nginx.conf
   sudo /etc/init.d/nginx restart
 }
 
